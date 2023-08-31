fbce4d469cb3e166edcc92f8611f417c11d6dcf7
Mike Ross <mikeross36mikeross@gmail.com>
2023-08-31 15:16:48 +0200

    sigup bug fixed

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,18 +1,47 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { signupUserAction } from "../actions/authActions";
 import Button from "../components/Button";
 
+const signupId = "signupId";
+
 export default function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const dispatch = useDispatch();
+
+  function handleSignupUser(e) {
+    e.preventDefault();
+    if (password !== passwordConfirm) {
+      toast.error("Passwords do not match!", { toastId: signupId });
+      return;
+    }
+    dispatch(signupUserAction(name, email, password));
+    setName("");
+    setEmail("");
+    setPassword("");
+    setPasswordConfirm("");
+  }
+
   return (
     <section className="signup">
       <div className="signup__form-container">
         <h2 className="signup__title">create account</h2>
-        <form className="signup__form">
+        <form className="signup__form" onSubmit={handleSignupUser}>
           <div className="input__control">
             <input
               type="text"
               className="form__input"
               placeholder="your username"
               name="username"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="input__control">
@@ -21,6 +50,9 @@ export default function Signup() {
               className="form__input"
               placeholder="some@email.com"
               name="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="input__control">
@@ -29,6 +61,9 @@ export default function Signup() {
               className="form__input"
               placeholder="your password"
               name="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="input__control">
@@ -37,6 +72,9 @@ export default function Signup() {
               className="form__input"
               placeholder="confirm password"
               name="passwordConfirm"
+              required
+              value={passwordConfirm}
+              onChange={(e) => setPasswordConfirm(e.target.value)}
             />
           </div>
           <Button
